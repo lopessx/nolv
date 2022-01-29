b07c83ffde682e44621beae7d314d55339158a6b
lopessx <warlopess@gmail.com>
2022-01-29 15:44:16 -0300

    feat: Implementado controller de produto

--- api/app/Http/Controllers/ProductsController.php
+++ api/app/Http/Controllers/ProductsController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Client\Request;
+use App\Models\Products;
+use Exception;
+use Illuminate\Http\Request;
 
 class ProductsController extends Controller {
 	/**
@@ -15,22 +17,69 @@ class ProductsController extends Controller {
 	}
 
 	public function get(Request $request) {
-		# code...
+		try {
+			$products = Products::all();
+
+			return response(['success' => true, 'products' => $products]);
+		} catch (Exception $e) {
+			return response(['message' => $e->getMessage(), 'code' => $e->getCode()], 404);
+		}
 	}
 
-	public function getOne(Request $request) {
-		# code...
+	public function getOne(Request $request, $id) {
+		try {
+			$id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
+			$product = Products::findOrFail($id);
+
+
+			return response(['success' => true, 'product' => $product]);
+		} catch (Exception $e) {
+			return response(['message' => $e->getMessage(), 'code' => $e->getCode()], 404);
+		}
 	}
 
 	public function store(Request $request) {
-		# code...
+		try {
+			$product = new Products();
+			$product->store_id = filter_var($request->storeId, FILTER_SANITIZE_NUMBER_INT);
+			$product->category_id = filter_var($request->categoryId, FILTER_SANITIZE_NUMBER_INT);
+			$product->name = filter_var($request->name, FILTER_SANITIZE_STRING);
+			$product->price = filter_var($request->price, FILTER_SANITIZE_NUMBER_FLOAT);
+			$product->file_path = filter_var($request->path, FILTER_SANITIZE_STRING);
+			$product->save();
+
+
+			return response(['success' => true]);
+		} catch (Exception $e) {
+			return response(['message' => $e->getMessage(), 'code' => $e->getCode()], 404);
+		}
 	}
 
-	public function update(Request $request) {
-		# code...
+	public function update(Request $request, $id) {
+		try {
+			$id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
+			$product = Products::findOrFail($id);
+			$product->store_id = filter_var($request->storeId, FILTER_SANITIZE_NUMBER_INT);
+			$product->category_id = filter_var($request->categoryId, FILTER_SANITIZE_NUMBER_INT);
+			$product->name = filter_var($request->name, FILTER_SANITIZE_STRING);
+			$product->price = filter_var($request->price, FILTER_SANITIZE_NUMBER_FLOAT);
+			$product->file_path = filter_var($request->path, FILTER_SANITIZE_STRING);
+			$product->save();
+
+			return response(['success' => true]);
+		} catch (Exception $e) {
+			return response(['message' => $e->getMessage(), 'code' => $e->getCode()], 404);
+		}
 	}
 
-	public function delete(Request $request) {
-		# code...
+	public function delete(Request $request, $id) {
+		try {
+			$id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
+			Products::findOrFail($id)->delete();
+
+			return response(['success' => true]);
+		} catch (Exception $e) {
+			return response(['message' => $e->getMessage(), 'code' => $e->getCode()], 404);
+		}
 	}
 }
