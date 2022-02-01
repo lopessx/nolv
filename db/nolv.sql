061159c03b82abadd3d838061b711a48d5a999dd
lopessx <warlopess@gmail.com>
2022-01-31 21:01:52 -0300

    chore: Atualizaçao de banco de dados utilizado

--- db/nolv.sql
+++ db/nolv.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Tempo de geração: 18/12/2021 às 15:14
+-- Tempo de geração: 01/02/2022 às 01:01
 -- Versão do servidor: 10.4.21-MariaDB
 -- Versão do PHP: 7.4.23
 
@@ -30,8 +30,15 @@ SET time_zone = "+00:00";
 CREATE TABLE `cards` (
   `id` int(11) NOT NULL,
   `client_id` int(11) NOT NULL,
-  `token` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `token` varchar(255) CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Despejando dados para a tabela `cards`
+--
+
+INSERT INTO `cards` (`id`, `client_id`, `token`) VALUES
+(1, 1, 'iahspdhaspodhapsod');
 
 -- --------------------------------------------------------
 
@@ -41,8 +48,16 @@ CREATE TABLE `cards` (
 
 CREATE TABLE `category` (
   `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `name` varchar(255) CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Despejando dados para a tabela `category`
+--
+
+INSERT INTO `category` (`id`, `name`) VALUES
+(1, 'Entretenimento'),
+(2, 'Finanças');
 
 -- --------------------------------------------------------
 
@@ -52,10 +67,51 @@ CREATE TABLE `category` (
 
 CREATE TABLE `clients` (
   `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `phone` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `name` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `phone` varchar(255) CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Despejando dados para a tabela `clients`
+--
+
+INSERT INTO `clients` (`id`, `name`, `email`, `phone`) VALUES
+(1, 'Jonas Silva', 'jonas123@alskla.su', '81993431392'),
+(2, 'João Ferreira', 'joao222@asasda.su', '81993431392]'),
+(3, 'Luan Silva', 'lsilva@aslkas.su', '81993431392');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `operational_system`
+--
+
+CREATE TABLE `operational_system` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Despejando dados para a tabela `operational_system`
+--
+
+INSERT INTO `operational_system` (`id`, `name`) VALUES
+(1, 'Linux'),
+(2, 'MacOS'),
+(3, 'Windows');
 
 -- --------------------------------------------------------
 
@@ -65,10 +121,17 @@ CREATE TABLE `clients` (
 
 CREATE TABLE `paymethods` (
   `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `type` varchar(255) NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `type` varchar(255) CHARACTER SET utf8 NOT NULL,
   `active` tinyint(1) DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Despejando dados para a tabela `paymethods`
+--
+
+INSERT INTO `paymethods` (`id`, `name`, `type`, `active`) VALUES
+(1, 'Pagseguro', 'card', 1);
 
 -- --------------------------------------------------------
 
@@ -80,10 +143,22 @@ CREATE TABLE `products` (
   `id` int(11) NOT NULL,
   `store_id` int(11) NOT NULL,
   `category_id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8 NOT NULL,
   `price` double DEFAULT 0,
-  `file_path` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `file_path` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `operational_system_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Despejando dados para a tabela `products`
+--
+
+INSERT INTO `products` (`id`, `store_id`, `category_id`, `name`, `price`, `file_path`, `operational_system_id`) VALUES
+(13, 1, 1, 'Sistema de gerenciamento de pizzaria acbdef abcdef acdef', 187.99, '/arquivo.txt', 1),
+(14, 1, 1, 'Gestão fácil', 89.54, '/arquivo.txt', 2),
+(15, 1, 1, 'Padafácil', 9.99, '/arquivo.txt', 3),
+(16, 1, 1, 'Farmapédia', 3.46, '/arquivo.txt', 1),
+(17, 1, 1, 'Opera PIP', 10, '/arquivo.txt', 2);
 
 -- --------------------------------------------------------
 
@@ -95,7 +170,7 @@ CREATE TABLE `products_sales` (
   `id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
   `sales_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -106,8 +181,8 @@ CREATE TABLE `products_sales` (
 CREATE TABLE `product_images` (
   `id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
-  `path` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `path` varchar(255) CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -118,8 +193,9 @@ CREATE TABLE `product_images` (
 CREATE TABLE `ratings` (
   `id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
-  `rating` int(11) DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `rating` int(11) DEFAULT 0,
+  `client_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -132,7 +208,14 @@ CREATE TABLE `sales` (
   `client_id` int(11) NOT NULL,
   `paymethod_id` int(11) NOT NULL,
   `total` double NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Despejando dados para a tabela `sales`
+--
+
+INSERT INTO `sales` (`id`, `client_id`, `paymethod_id`, `total`) VALUES
+(1, 1, 1, 29.49);
 
 -- --------------------------------------------------------
 
@@ -142,8 +225,17 @@ CREATE TABLE `sales` (
 
 CREATE TABLE `status_tickets` (
   `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `name` varchar(255) CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Despejando dados para a tabela `status_tickets`
+--
+
+INSERT INTO `status_tickets` (`id`, `name`) VALUES
+(1, 'Aberto'),
+(2, 'Respondido'),
+(3, 'Finalizado');
 
 -- --------------------------------------------------------
 
@@ -154,9 +246,16 @@ CREATE TABLE `status_tickets` (
 CREATE TABLE `stores` (
   `id` int(11) NOT NULL,
   `client_id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8 NOT NULL,
   `balance` double DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Despejando dados para a tabela `stores`
+--
+
+INSERT INTO `stores` (`id`, `client_id`, `name`, `balance`) VALUES
+(1, 3, 'Demais Eletronicos', 0);
 
 -- --------------------------------------------------------
 
@@ -168,9 +267,16 @@ CREATE TABLE `tickets` (
   `id` int(11) NOT NULL,
   `client_id` int(11) NOT NULL,
   `store_id` int(11) NOT NULL,
-  `message` varchar(255) NOT NULL,
+  `message` varchar(255) CHARACTER SET utf8 NOT NULL,
   `status_tickets_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Despejando dados para a tabela `tickets`
+--
+
+INSERT INTO `tickets` (`id`, `client_id`, `store_id`, `message`, `status_tickets_id`) VALUES
+(1, 1, 1, 'Deu problema com produto', 1);
 
 --
 -- Índices para tabelas despejadas
@@ -196,6 +302,18 @@ ALTER TABLE `clients`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Índices de tabela `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `operational_system`
+--
+ALTER TABLE `operational_system`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Índices de tabela `paymethods`
 --
 ALTER TABLE `paymethods`
@@ -207,7 +325,8 @@ ALTER TABLE `paymethods`
 ALTER TABLE `products`
   ADD PRIMARY KEY (`id`),
   ADD KEY `products_FK` (`store_id`),
-  ADD KEY `products_FK_1` (`category_id`);
+  ADD KEY `products_FK_1` (`category_id`),
+  ADD KEY `products_FK_2` (`operational_system_id`);
 
 --
 -- Índices de tabela `products_sales`
@@ -229,7 +348,8 @@ ALTER TABLE `product_images`
 --
 ALTER TABLE `ratings`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `ratings_FK` (`product_id`);
+  ADD KEY `ratings_FK` (`product_id`),
+  ADD KEY `ratings_FK_1` (`client_id`);
 
 --
 -- Índices de tabela `sales`
@@ -269,73 +389,85 @@ ALTER TABLE `tickets`
 -- AUTO_INCREMENT de tabela `cards`
 --
 ALTER TABLE `cards`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de tabela `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de tabela `clients`
 --
 ALTER TABLE `clients`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de tabela `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `operational_system`
+--
+ALTER TABLE `operational_system`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de tabela `paymethods`
 --
 ALTER TABLE `paymethods`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de tabela `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT de tabela `products_sales`
 --
 ALTER TABLE `products_sales`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de tabela `product_images`
 --
 ALTER TABLE `product_images`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de tabela `ratings`
 --
 ALTER TABLE `ratings`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de tabela `sales`
 --
 ALTER TABLE `sales`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de tabela `status_tickets`
 --
 ALTER TABLE `status_tickets`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de tabela `stores`
 --
 ALTER TABLE `stores`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de tabela `tickets`
 --
 ALTER TABLE `tickets`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Restrições para tabelas despejadas
@@ -352,7 +484,8 @@ ALTER TABLE `cards`
 --
 ALTER TABLE `products`
   ADD CONSTRAINT `products_FK` FOREIGN KEY (`store_id`) REFERENCES `stores` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `products_FK_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `products_FK_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `products_FK_2` FOREIGN KEY (`operational_system_id`) REFERENCES `operational_system` (`id`) ON DELETE CASCADE;
 
 --
 -- Restrições para tabelas `products_sales`
@@ -371,7 +504,8 @@ ALTER TABLE `product_images`
 -- Restrições para tabelas `ratings`
 --
 ALTER TABLE `ratings`
-  ADD CONSTRAINT `ratings_FK` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `ratings_FK` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `ratings_FK_1` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`) ON DELETE CASCADE;
 
 --
 -- Restrições para tabelas `sales`
