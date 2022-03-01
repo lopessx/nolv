0ff6ef6afea46c4c710ac34bea22b98b2afaa884
lopessx <warlopess@gmail.com>
2022-03-01 11:59:41 -0300

    feat: implementado barras de carregamento

--- front/quasar.conf.js
+++ front/quasar.conf.js
@@ -89,7 +89,12 @@ module.exports = configure(function (ctx) {
 
     // https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-framework
     framework: {
-      config: {},
+      config: {
+        loadingBar: {
+          size: '4px',
+          color: 'accent'
+        }
+      },
 
       // iconSet: 'material-icons', // Quasar icon set
       // lang: 'en-US', // Quasar language pack
@@ -102,7 +107,12 @@ module.exports = configure(function (ctx) {
       // directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: [
+        'LocalStorage',
+        'SessionStorage',
+        'Loading',
+        'LoadingBar'
+      ]
     },
 
     // animations: 'all', // --- includes all animations
