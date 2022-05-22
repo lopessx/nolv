2a97790d9e7f02c68b934db9b93d1dde0ed04d05
lopessx <warlopess@gmail.com>
2022-05-21 22:00:44 -0300

    improvement: Adicionar rotas sensíveis ao grupo de rotas protegidas por autenticação

--- api/routes/web.php
+++ api/routes/web.php
@@ -13,53 +13,60 @@
 |
 */
 
-// Products
-$router->get('/products', 'ProductController@get');
-$router->get('/product/{id}', 'ProductController@getOne');
-$router->post('/product', 'ProductController@store');
-$router->put('/product/{id}', 'ProductController@update');
-$router->delete('/product/{id}', 'ProductController@delete');
-$router->post('/product/upload', 'ProductController@uploadProduct');
-$router->post('/product/image/upload', 'ProductController@storeImages');
-$router->delete('/product/image/delete/{imgId}', 'ProductController@deleteProductImage');
-$router->get('/products/client/{id}', 'OrderController@getProductsClient');
-$router->get('/product/download/{id}', 'ProductController@downloadProduct');
-
-// Clients
+// Routes with throttle
 $router->group(['middleware' => 'throttle:global'], function () use ($router) {
+	// Clients
 	$router->post('/client/login', 'ClientController@login');
 	$router->post('/client/auth', 'ClientController@auth');
 	$router->post('/client/register', 'ClientController@register');
 });
 
 $router->group(['middleware' => 'auth'], function () use ($router) {
-	$router->get('/', function () {
-		return 1;
-	});
-
+	// Clients
 	$router->post('/logout', 'ClientController@logout');
+	$router->delete('/client/delete/{id}', 'ClientController@delete');
+	$router->put('/client/update/{id}', 'ClientController@update');
+
+	// Products
+	$router->post('/product', 'ProductController@store');
+	$router->put('/product/{id}', 'ProductController@update');
+	$router->delete('/product/{id}', 'ProductController@delete');
+	$router->post('/product/upload', 'ProductController@uploadProduct');
+	$router->post('/product/image/upload', 'ProductController@storeImages');
+	$router->delete('/product/image/delete/{imgId}', 'ProductController@deleteProductImage');
+	$router->get('/products/client/{id}', 'OrderController@getProductsClient');
+	$router->get('/product/download/{id}', 'ProductController@downloadProduct');
+
+	// Stores
+	$router->delete('/store/{storeId}', 'StoreController@delete');
+	$router->get('/store/client/{clientId}', 'StoreController@getClientStore');
+	$router->post('/store', 'StoreController@store');
+	$router->put('/store/{storeId}', 'StoreController@update');
+
+	// Tickets
+	$router->post('/ticket', 'TicketController@store');
+
+	// Orders
+	$router->get('/order', 'OrderController@get');
+	$router->get('/orders/client/{id}', 'OrderController@getOrdersClient');
+	$router->get('/order/{id}', 'OrderController@show');
+	$router->post('/order', 'OrderController@store');
+	$router->put('/order/{id}', 'OrderController@update');
+	$router->delete('/order/{id}', 'OrderController@delete');
+
+	// Product Ratings
+	$router->get('/rating/product/{pid}/client/{cid}', 'RatingController@getClientRating');
+	$router->post('/rating', 'RatingController@store');
+	$router->put('/rating/{id}', 'RatingController@update');
+	$router->delete('/rating/{id}', 'RatingController@delete');
+
+	// Payment methods
+	$router->post('/payment/capture/{orderId}', 'PaymethodController@capturePayment');
 });
 
-
-$router->delete('/client/delete/{id}', 'ClientController@delete');
-$router->put('/client/update/{id}', 'ClientController@update');
-
-// Stores
-$router->delete('/store/{storeId}', 'StoreController@delete');
-$router->get('/store/client/{clientId}', 'StoreController@getClientStore');
-$router->post('/store', 'StoreController@store');
-$router->put('/store/{storeId}', 'StoreController@update');
-
-// Tickets
-$router->post('/ticket', 'TicketController@store');
-
-// Orders
-$router->get('/order', 'OrderController@get');
-$router->get('/orders/client/{id}', 'OrderController@getOrdersClient');
-$router->get('/order/{id}', 'OrderController@show');
-$router->post('/order', 'OrderController@store');
-$router->put('/order/{id}', 'OrderController@update');
-$router->delete('/order/{id}', 'OrderController@delete');
+// Products
+$router->get('/products', 'ProductController@get');
+$router->get('/product/{id}', 'ProductController@getOne');
 
 // Languages
 $router->get('/languages', 'LanguageController@get');
@@ -75,11 +82,6 @@ $router->get('/product/image/{id}', 'ProductImageController@getOne');
 
 // Product Ratings
 $router->get('/ratings/product/{id}', 'RatingController@getProductRatings');
-$router->get('/rating/product/{pid}/client/{cid}', 'RatingController@getClientRating');
-$router->post('/rating', 'RatingController@store');
-$router->put('/rating/{id}', 'RatingController@update');
-$router->delete('/rating/{id}', 'RatingController@delete');
 
 // Payment Methods
 $router->get('/payment/list', 'PaymethodController@get');
-$router->post('/payment/capture/{orderId}', 'PaymethodController@capturePayment');
